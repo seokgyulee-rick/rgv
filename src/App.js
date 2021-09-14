4cec6b2fa7db59754fc4d515c2ddca4e7145d9a6
Rick <dltjrrb0729@naver.com>
2021-09-14 12:58:05 +0900

    data is fully fetched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -17,14 +17,34 @@ class App extends React.Component {
         data: { movies },
       },
     } = await axios.get(
-      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
     );
+    console.log(typeof movies);
+    console.log(movies);
     this.setState({ movies, isLoading: false });
     //isLoading = movies
   };
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "is loading..." : "ready"}</div>;
+    const { movies, isLoading } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "loading..."
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  year={movie.year}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
